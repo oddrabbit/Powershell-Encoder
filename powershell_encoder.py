aad760e03f2d839549585b7ecc2707afacb4c630
OddRabbit <52036269+oddrabbit@users.noreply.github.com>
2022-09-11 02:00:18 +1000

    Update powershell_encoder.py

--- powershell_encoder.py
+++ powershell_encoder.py
@@ -11,14 +11,12 @@ You should have received a copy of the GNU General Public License along with thi
 see <https://www.gnu.org/licenses/>. 
 '''
 
-import argparse
-import base64
-import sys
-
 def argument_parser():
     parser = argparse.ArgumentParser(description = "Script to create a base64 Powershell one-liner")
     parser.add_argument("-c", type = str, help = "Plaintext command to base64 encode", required = False)
     parser.add_argument("--command", type = str, help = "Plaintext command to base64 encode", required = False)
+    parser.add_argument("-f", type = str, help = "File including command to base64 encode", required = False)
+    parser.add_argument("--file", type = str, help = "File including command to base64 encode", required = False)
     args = parser.parse_args()
     return args
 
@@ -29,6 +27,20 @@ def main():
 
     if args.c or args.command:
         powershell_encoded_string = powershell_launcher_string + base64.b64encode(args.c.encode("UTF-16LE")).decode("utf-8")
+        print(powershell_encoded_string)
+        quit()
+
+    if args.f:
+        file_handler = open(args.f, "r")
+        file_handler_content = file_handler.readlines()
+        file_handler_content = "\n".join(str(i) for i in file_handler_content)
+        powershell_encoded_string = powershell_launcher_string + base64.b64encode(file_handler_content.encode("UTF-16LE")).decode("utf-8")
+        print(powershell_encoded_string)
+    elif args.file:
+        file_handler = open(args.file, "r")
+        file_handler_content = file_handler.readlines()
+        file_handler_content = "\n".join(str(i) for i in file_handler_content)
+        powershell_encoded_string = powershell_launcher_string + base64.b64encode(file_handler_content.encode("UTF-16LE")).decode("utf-8")
         print(powershell_encoded_string)
 
 if __name__ == "__main__":

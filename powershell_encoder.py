ac65c3062a29d242604385aea72fba698f01095b
OddRabbit <52036269+oddrabbit@users.noreply.github.com>
2022-09-11 01:04:48 +1000

    Update powershell_encoder.py

--- powershell_encoder.py
+++ powershell_encoder.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 import base64
-import sys
+import argparse
+
+parser = argparse.ArgumentParser(description = "Script to create a base64 Powershell one-liner")
+parser.add_argument("-c", type = str, required = True)
+parser.add_argument("--command", type = str, required = True)
+args = parser.parse_args()
 
 powershell_launcher_string = "powershell.exe -version 2 -exec bypass -enc "
-powershell_plain_string = sys.argv[1]
 
-powershell_encoded_string = powershell_launcher_string + base64.b64encode(powershell_plain_string.encode("UTF-16LE")).decode("utf-8")
+if args.c:
+    powershell_encoded_string = powershell_launcher_string + base64.b64encode(args.c.encode("UTF-16LE")).decode("utf-8")
+elif args.command:
+    powershell_encoded_string = powershell_launcher_string + base64.b64encode(args.command.encode("UTF-16LE")).decode("utf-8")
 
 print(powershell_encoded_string)
